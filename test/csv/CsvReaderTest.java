9d51af61eb97dcc82e8d018c82cb5b3f4a565579
Michael Faes <rolve@trick17.ch>
2018-09-26 15:02:52 +0200

    Fix test

--- test/csv/CsvReaderTest.java
+++ test/csv/CsvReaderTest.java
@@ -1,53 +1,41 @@
 package csv;
 
-import static csv.CsvReader.Separator.COMMA;
-import static java.util.Arrays.asList;
+import static org.apache.commons.csv.CSVFormat.EXCEL;
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import java.io.StringReader;
 
-import csv.Column;
-import csv.CsvReader;
+import org.junit.Test;
 
 public class CsvReaderTest {
 
     @Test
-    public void testRead() throws ReflectiveOperationException {
-        var data = asList(
-                "Name,E-Mail-Adresse",
-                "Hans-Peter Moll,hpmoll@example.com",
-                "Frieda Graf,graff@example.com");
+    public void testRead() throws Exception {
+        var data = 
+                "Name,E-Mail-Adresse\r\n" +
+                "Hans-Peter Moll,hpmoll@example.com\r\n" +
+                "Frieda Graf,graff@example.com\r\n";
 
-        var reader = new CsvReader(COMMA);
-        var students = reader.read(data, Student.class);
+        var reader = new CsvReader(EXCEL.withHeader());
+        var students = reader.read(new StringReader(data), Student.class);
 
         assertEquals(2, students.size());
         assertEquals("Hans-Peter Moll", students.get(0).name);
         assertEquals("Hans-Peter Moll", students.get(0).nameAgain);
         assertEquals("hpmoll@example.com", students.get(0).email);
-        assertEquals(null, students.get(0).nethz);
-        assertEquals(null, students.get(0).nethzAgain);
         assertEquals("Frieda Graf", students.get(1).name);
         assertEquals("Frieda Graf", students.get(1).nameAgain);
         assertEquals("graff@example.com", students.get(1).email);
-        assertEquals(null, students.get(1).nethz);
-        assertEquals(null, students.get(1).nethzAgain);
     }
 
     public static class Student {
         @Column("Name")
         private String name;
 
-        @ColumnIndex(0)
+        @Column("Name")
         private String nameAgain;
 
         @Column("E-Mail-Adresse")
         private String email;
-
-        @Column("NETHZ")
-        private String nethz;
-
-        @ColumnIndex(3)
-        private String nethzAgain;
     }
 }
