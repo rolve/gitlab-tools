fe15b6bb622c441a74d385bc482a7b91001af741
Michael Faes <rolve@trick17.ch>
2018-11-05 10:38:13 -0500

    A cmd to publish grades

--- src/GitlabToolsCli.java
+++ src/GitlabToolsCli.java
@@ -15,6 +15,7 @@ public class GitlabToolsCli {
         put("assign-members", AssignMembersCmd.class);
         put("publish", PublishCmd.class);
         put("publish-fast", PublishFastCmd.class);
+        put("publish-grades", PublishGradesCmd.class);
         put("clone-all", CloneAllCmd.class);
     }};
 
---
+++ src/PublishGradesCmd.java
@@ -0,0 +1,75 @@
+import static com.lexicalscope.jewel.cli.CliFactory.createCli;
+import static java.nio.file.Files.lines;
+import static java.util.stream.Collectors.joining;
+
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Paths;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.gitlab4j.api.models.RepositoryFile;
+
+import com.lexicalscope.jewel.cli.Option;
+
+public class PublishGradesCmd extends Cmd<PublishGradesCmd.Args> {
+
+    public PublishGradesCmd(String[] rawArgs) throws Exception {
+        super(createCli(Args.class).parseArguments(rawArgs));
+    }
+
+    @Override
+    void call() throws Exception {
+        var mainGroup = getGroup(args.getGroupName());
+        var studGroup = getSubGroup(mainGroup, "students");
+
+        CSVParser parser = CSVFormat.newFormat(';').withHeader()
+                .parse(new FileReader(args.getGradesFile()));
+        String appendix = lines(Paths.get(args.getAppendixFile()))
+                .collect(joining("\n"));
+
+        var fileApi = gitlab.getRepositoryFileApi();
+
+        int created = 0;
+        int existing = 0;
+        for (var record : parser) {
+            var builder = new StringBuilder();
+            for (var col : parser.getHeaderMap().keySet()) {
+                builder.append(col + (col.isEmpty() ? "" : ": ") + record.get(col)).append("\n");
+            }
+            builder.append("\n").append(appendix);
+
+            var name = record.get("Name");
+            var project = getProject(studGroup, name);
+
+            var path = args.getProjectName() + "/grade.txt";
+            if (fileApi.getOptionalFileInfo(project.getId(), path, "master").isPresent()) {
+                existing++;
+            } else {
+                var file = new RepositoryFile();
+                file.setContent(builder.toString());
+                file.setFilePath(path);
+                fileApi.createFile(file, project.getId(), "master", "publish grades for " + args.getProjectName());
+                created++;
+            }
+            if ((existing + created) % 10 == 0) {
+                System.out.printf("%d processed\n", existing + created);
+            }
+        }
+        System.out.printf("Done. %d published, %d already exist.\n", created, existing);
+    }
+
+    interface Args extends Cmd.Args {
+        @Option
+        String getGroupName();
+        
+        @Option
+        String getProjectName();
+
+        @Option
+        File getGradesFile();
+
+        @Option
+        String getAppendixFile();
+    }
+}
