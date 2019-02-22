8609a0dd1b64db55680bef1eea2c6bf8b9fffc80
Michael Faes <rolve@trick17.ch>
2019-02-22 14:04:07 +0100

    Find occurrences of name and NETHZ in sources, to anonymize

--- src/ExportSourcesCmd.java
+++ src/ExportSourcesCmd.java
@@ -1,14 +1,17 @@
 import static com.lexicalscope.jewel.cli.CliFactory.createCli;
+import static java.nio.charset.StandardCharsets.*;
 import static java.nio.file.Files.*;
 import static java.util.Collections.reverseOrder;
+import static java.util.stream.Collectors.toList;
 import static org.eclipse.jgit.api.Git.cloneRepository;
 import static org.eclipse.jgit.api.Git.open;
 
 import java.io.IOException;
+import java.nio.charset.CharacterCodingException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.util.List;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -18,7 +21,7 @@ import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 import com.lexicalscope.jewel.cli.Option;
 
-public class ExportSourcesCmd extends Cmd<ExportSourcesCmd.Args> {
+public class ExportSourcesCmd extends CmdWithEdoz<ExportSourcesCmd.Args> {
 
     private CredentialsProvider credentials;
     private int cloned;
@@ -49,11 +52,16 @@ public class ExportSourcesCmd extends Cmd<ExportSourcesCmd.Args> {
 		    removeNonSources(repoDir);
 		    removeEmptyDirs(repoDir);
 
+		    var student = students.stream()
+		            .filter(s -> s.nethz.get().equals(project.getName()))
+		            .findFirst();
+		    if (student.isPresent()) {
+		        findNameInSources(repoDir, student.get());
+		    } else {
+		        System.err.println("No EDoz entry for " + project.getName());
+		    }
+
 			exported++;
-			System.out.print(".");
-			if (exported % 80 == 0) {
-                System.out.println();
-            }
 		}
 		System.out.printf("Done. %d submissions checked out (%d newly cloned)\n",
 		        exported, cloned);
@@ -138,6 +146,33 @@ public class ExportSourcesCmd extends Cmd<ExportSourcesCmd.Args> {
         }
     }
 
+    private void findNameInSources(Path repoDir, EdozStudent student) throws IOException {
+        try (var files = walk(repoDir).filter(Files::isRegularFile)) {
+            for (var f : iterable(files)) {
+                var lines = readAllLinesRobust(f);
+                var matching = lines.stream().filter(l ->
+                        l.contains(student.firstName) ||
+                        l.contains(student.lastName) ||
+                        l.contains(student.nethz.get())).collect(toList());
+                if (!matching.isEmpty()) {
+                    System.out.println(f);
+                    matching.stream().map(l -> "    " + l).forEach(System.out::println);
+                }
+            }
+        }
+    }
+
+    private List<String> readAllLinesRobust(Path f) throws IOException {
+        for (var charset : List.of(UTF_8, ISO_8859_1, US_ASCII)) {
+            try {
+                return readAllLines(f, charset);
+            } catch (CharacterCodingException e) {
+                // try next
+            }
+        }
+        throw new AssertionError("no matching encoding found for " + f);
+    }
+
     private void deleteRecursive(Path path) throws IOException {
         try (var paths = Files.walk(path).sorted(reverseOrder())) {
             for (var p : iterable(paths)) {
@@ -147,11 +182,7 @@ public class ExportSourcesCmd extends Cmd<ExportSourcesCmd.Args> {
         }
     }
 
-    private <T> Iterable<T> iterable(Stream<T> stream) {
-        return () -> stream.iterator();
-    }
-
-	interface Args extends Cmd.Args {
+	interface Args extends CmdWithEdoz.Args {
 		@Option
 		String getGroupName();
 
