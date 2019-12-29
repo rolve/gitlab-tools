91b8c1db0df2a776598896761ed2ffaaa5acbe9c
Michael Faes <rolve@trick17.ch>
2019-12-29 16:07:56 +0100

    A batch mode! Every argument after --batch is considered a complete list of arguments and will be split and appended to the list of arguments before --batch.

--- src/GitlabToolsCli.java
+++ src/GitlabToolsCli.java
@@ -1,10 +1,11 @@
 import static java.util.Arrays.stream;
 import static java.util.Map.entry;
+import static java.util.regex.Pattern.compile;
 import static java.util.stream.Collectors.joining;
-import static org.apache.commons.lang3.ArrayUtils.subarray;
+import static java.util.stream.Collectors.toList;
+import static org.apache.commons.lang3.ArrayUtils.*;
 
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class GitlabToolsCli {
 
@@ -29,26 +30,67 @@ public class GitlabToolsCli {
             System.err.println("no command specified");
             return;
         }
-
-        var cmdClass = commands.get(args[0]);
-        if (cmdClass == null) {
-            System.err.println("unknown command " + args[0]);
+        var cmdName = args[0];
+        if (!commands.containsKey(cmdName)) {
+            System.err.println("unknown command " + cmdName);
+            return;
         }
 
-        // To prevent accidental execution of a command within Eclipse (and possibly
-        // disastrous consequences), require confirmation.
-        confirm(args);
-
-        var cmd = cmdClass.getConstructor(String[].class)
-                .newInstance(new Object[] { subarray(args, 1, args.length) });
-        cmd.execute();
+        int batchIndex = indexOf(args, "--batch");
+        if (batchIndex == INDEX_NOT_FOUND) {
+            var cmdArgs = subarray(args, 1, args.length);
+            confirm(cmdName, List.<String[]>of(cmdArgs));
+            execute(cmdName, cmdArgs);
+        } else {
+            var constArgs = subarray(args, 1, batchIndex);
+            var varArgs = subarray(args, batchIndex + 1, args.length);
+            var completeArgs = stream(varArgs)
+                    .map(a -> addAll(constArgs, split(a)))
+                    .collect(toList());
+            confirm(cmdName, completeArgs);
+            for (var cmdArgs : completeArgs) {
+                execute(cmdName, cmdArgs);
+            }
+        }
     }
 
+    /**
+     * To prevent accidental execution of a command within Eclipse (and possibly
+     * disastrous consequences), require confirmation.
+     */
     @SuppressWarnings("resource")
-    private static boolean confirm(String[] args) {
-        System.out.println("About to execute " + stream(args).collect(joining(" ")));
+    private static boolean confirm(String cmd, List<String[]> argsList) {
+        System.out.print("About to execute" + (argsList.size() == 1 ? " " : "\n"));
+        for (var args : argsList) {
+            System.out.println(cmd + " " + stream(args).collect(joining(" ")));
+        }
         System.out.print("Press Enter to continue.");
         new Scanner(System.in).nextLine();
         return true;
+    }
+
+    private static String[] split(String args) {
+        var doubleQuot = "\"([^\"\\\\]*(?:\\\\.[^\"\\\\]*)*)\"";
+        var singleQuot = "'([^'\\\\]*(?:\\\\.[^'\\\\]*)*)'";
+        var spaceless = "[^\\s]+";
+        var regex = compile(doubleQuot + "|" + singleQuot + "|" + spaceless);
+        var matcher = regex.matcher(args);
+        var list = new ArrayList<String>();
+        while (matcher.find()) {
+            if (matcher.group(1) != null) {
+                list.add(matcher.group(1));
+            } else if (matcher.group(2) != null) {
+                list.add(matcher.group(2));
+            } else {
+                list.add(matcher.group());
+            }
+        }
+        return list.toArray(String[]::new);
+    }
+
+    private static void execute(String name, String[] args) throws Exception {
+        var cmd = commands.get(name).getConstructor(String[].class)
+                .newInstance(new Object[] { args });
+        cmd.execute();
     }
 }
