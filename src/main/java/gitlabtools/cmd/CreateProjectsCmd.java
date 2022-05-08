7546d0ebd607f7ea6f4e683137c531b289732760
Michael Faes <rolve@trick17.ch>
2022-05-08 16:49:12 +0200

    Course file is now a simple text file with one username (or email address) per line

--- src/main/java/gitlabtools/cmd/CreateProjectsCmd.java
+++ src/main/java/gitlabtools/cmd/CreateProjectsCmd.java
@@ -1,30 +1,24 @@
 package gitlabtools.cmd;
 
 import com.lexicalscope.jewel.cli.Option;
-import csv.CsvReader;
-import gitlabtools.Student;
 import org.gitlab4j.api.models.AccessLevel;
 import org.gitlab4j.api.models.Project;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 import static com.lexicalscope.jewel.cli.CliFactory.createCli;
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
-import static org.apache.commons.csv.CSVFormat.TDF;
 
 public class CreateProjectsCmd extends Cmd<CreateProjectsCmd.Args> {
 
-    private final List<Student> students;
     private final AccessLevel access;
 
     public CreateProjectsCmd(String[] rawArgs) throws Exception {
         super(createCli(Args.class).parseArguments(rawArgs));
-        students = new CsvReader<>(TDF.withHeader(), Student.class)
-                .readAll(Path.of(args.getCourseFile()));
-        for (var student : students) {
-            student.normalizeUsername();
-        }
         access = AccessLevel.valueOf(args.getDefaultBranchAccess().toUpperCase());
     }
 
@@ -38,10 +32,11 @@ public class CreateProjectsCmd extends Cmd<CreateProjectsCmd.Args> {
                 .map(Project::getName)
                 .collect(toSet());
 
+        var students = readCourseFile();
+
         System.out.println("Creating projects for " + students.size() + " "
                 + (students.size() > 1 ? "people" : "person") + "...");
-        for (var student : students) {
-            var projectName = student.username;
+        for (var projectName : students) {
             if (args.getProjectNamePrefix() != null) {
                 if (args.getProjectNamePrefix().contains("_")) {
                     throw new AssertionError("illegal prefix; must not contain _");
@@ -68,8 +63,29 @@ public class CreateProjectsCmd extends Cmd<CreateProjectsCmd.Args> {
         }
     }
 
+    private List<String> readCourseFile() throws IOException {
+        try (var lines = Files.lines(Path.of(args.getCourseFile()))) {
+            return lines
+                    .map(this::normalizeUsername)
+                    .collect(toList());
+        }
+    }
+
+    private String normalizeUsername(String raw) {
+        var parts = raw.split("@");
+        if (parts.length == 1) {
+            return raw;
+        }
+        if (parts.length == 2) {
+            // username is an email address; use only first part
+            return parts[0];
+        } else {
+            throw new RuntimeException("invalid username in course file: " + raw);
+        }
+    }
+
     public interface Args extends gitlabtools.cmd.Args {
-        @Option(defaultValue = "course.txt") // tab-separated
+        @Option(defaultValue = "course.txt") // one username or email address per line
         String getCourseFile();
 
         @Option(defaultValue = "developer", pattern = "developer|maintainer|admin")
