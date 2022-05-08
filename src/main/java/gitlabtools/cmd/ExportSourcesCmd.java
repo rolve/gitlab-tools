287a1501f7e912c1a99aa7051af354814ec018cd
Michael Faes <rolve@trick17.ch>
2022-05-08 16:40:12 +0200

    Remove anonymization for now, to further simplify code

--- src/main/java/gitlabtools/cmd/ExportSourcesCmd.java
+++ src/main/java/gitlabtools/cmd/ExportSourcesCmd.java
@@ -1,54 +1,32 @@
 package gitlabtools.cmd;
 
 import com.lexicalscope.jewel.cli.Option;
-import csv.CsvReader;
-import gitlabtools.Student;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.TransportException;
 import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-import spoon.Launcher;
-import spoon.reflect.code.CtComment;
-import spoon.reflect.code.CtJavaDoc;
-import spoon.reflect.code.CtLiteral;
-import spoon.reflect.visitor.CtScanner;
-import spoon.reflect.visitor.DefaultJavaPrettyPrinter;
-import spoon.support.compiler.VirtualFile;
 
 import java.io.IOException;
-import java.nio.charset.CharacterCodingException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.regex.Pattern;
 
 import static com.lexicalscope.jewel.cli.CliFactory.createCli;
-import static java.nio.charset.StandardCharsets.*;
 import static java.nio.file.Files.*;
 import static java.util.Collections.reverseOrder;
 import static java.util.Collections.shuffle;
-import static java.util.regex.Pattern.compile;
-import static java.util.regex.Pattern.quote;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.IntStream.range;
-import static org.apache.commons.csv.CSVFormat.TDF;
 import static org.eclipse.jgit.api.Git.cloneRepository;
 import static org.eclipse.jgit.api.Git.open;
 
 public class ExportSourcesCmd extends Cmd<ExportSourcesCmd.Args> {
 
-    private final List<Student> students;
     private CredentialsProvider credentials;
 
     public ExportSourcesCmd(String[] rawArgs) throws Exception {
         super(createCli(Args.class).parseArguments(rawArgs));
-        students = new CsvReader<>(TDF.withHeader(), Student.class)
-                .readAll(Path.of(args.getCourseFile()));
-        for (var student : students) {
-            student.normalizeUsername();
-        }
     }
 
     @Override
@@ -73,19 +51,6 @@ public class ExportSourcesCmd extends Cmd<ExportSourcesCmd.Args> {
             removeNonSubmissions(repoDir);
             removeNonSources(repoDir);
             removeEmptyDirs(repoDir);
-
-            var student = students.stream()
-                    .filter(s -> s.username.equals(project.getName()))
-                    .findFirst();
-            if (student.isPresent()) {
-                var stud = student.get();
-                var tokens = List.of(stud.username, stud.firstName, stud.lastName);
-                anonymizeSources(repoDir, tokens);
-            } else {
-                progress.additionalInfo("not anonymized");
-                progress.interrupt();
-                System.out.println("Warning: no course file entry for " + project.getName());
-            }
 
             move(repoDir, destDir.resolve(newNames.next()));
             progress.advance();
@@ -181,116 +146,7 @@ public class ExportSourcesCmd extends Cmd<ExportSourcesCmd.Args> {
         }
     }
 
-    private void anonymizeSources(Path repoDir, List<String> tokens) throws IOException {
-        try (var files = walk(repoDir).filter(Files::isRegularFile)) {
-            for (var f : iterable(files)) {
-                if (!linesWithName(f, tokens).isEmpty()) {
-                    try {
-                        var code = String.join("\n", readAllLinesRobust(f));
-                        var anonymized = anonymize(code, tokens);
-                        write(f, List.of(anonymized), UTF_8);
-                    } catch (RuntimeException e) {
-                        System.err.println("issues with " + repoDir.getFileName());
-                        e.printStackTrace();
-                    }
-                }
-                // if name is still somewhere, report
-                var lines = linesWithName(f, tokens);
-                if (!lines.isEmpty()) {
-                    System.out.println(f + " contains one of " +
-                            String.join(", ", tokens));
-                    lines.stream().map(l -> "    " + l.trim()).forEach(System.out::println);
-                }
-            }
-        }
-    }
-
-    private List<String> linesWithName(Path f, List<String> tokens) throws IOException {
-        var patterns = toPatterns(tokens);
-        return readAllLinesRobust(f).stream()
-                .filter(l -> patterns.stream().anyMatch(p -> p.matcher(l).find()))
-                .collect(toList());
-    }
-
-    private String anonymize(String code, List<String> tokens) {
-        var spoon = new Launcher();
-        spoon.addInputResource(new VirtualFile(code));
-        spoon.getEnvironment().setNoClasspath(true);
-        spoon.getEnvironment().setAutoImports(true);
-        var types = spoon.buildModel().getAllTypes();
-
-        var builder = new StringBuilder();
-        for (var type : types) {
-            type.accept(new CtScanner() {
-                public void visitCtComment(CtComment comment) {
-                    super.visitCtComment(comment);
-                    handleComment(comment);
-                }
-                public void visitCtJavaDoc(CtJavaDoc javaDoc) {
-                    super.visitCtJavaDoc(javaDoc);
-                    handleComment(javaDoc);
-                }
-                private void handleComment(CtComment comment) {
-                    var anonymized = anonymizeString(comment.getContent(), tokens);
-                    // work around Spoon bug
-                    if (anonymized.startsWith("/")) {
-                        anonymized = "\n" + anonymized;
-                    }
-                    comment.setContent(anonymized);
-                }
-                public <T> void visitCtLiteral(CtLiteral<T> literal) {
-                    super.visitCtLiteral(literal);
-                    if (literal.getType().getQualifiedName().equals("java.lang.String")) {
-                        @SuppressWarnings("unchecked")
-                        T anonymized = (T) anonymizeString((String) literal.getValue(), tokens);
-                        literal.setValue(anonymized);
-                    }
-                }
-            });
-            var printer = new DefaultJavaPrettyPrinter(spoon.getEnvironment());
-            type.accept(printer);
-            builder.append(printer.getResult());
-            builder.append("\n\n");
-        }
-        return builder.toString();
-    }
-
-    private String anonymizeString(String content, List<String> tokens) {
-        var patterns = toPatterns(tokens);
-        var cleaned = content;
-        for (var pattern : patterns) {
-            cleaned = pattern.matcher(cleaned).replaceAll("???");
-        }
-        return cleaned;
-    }
-
-    private List<Pattern> toPatterns(List<String> tokens) {
-        return tokens.stream().map(t -> {
-            if (t.length() <= 3) {
-                // short tokens are only considered when they appear as a whole,
-                // i.e., not as part of a longer word
-                return compile("(?i)\\b" + quote(t) + "\\b");
-            } else {
-                return compile("(?i)" + quote(t));
-            }
-        }).collect(toList());
-    }
-
-    private List<String> readAllLinesRobust(Path f) throws IOException {
-        for (var charset : List.of(UTF_8, ISO_8859_1, US_ASCII)) {
-            try {
-                return readAllLines(f, charset);
-            } catch (CharacterCodingException e) {
-                // try next
-            }
-        }
-        throw new AssertionError("no matching encoding found for " + f);
-    }
-
     interface Args extends gitlabtools.cmd.Args {
-        @Option(defaultValue = "course.txt") // tab-separated
-        String getCourseFile();
-
         @Option
         String getDestinationDir();
     }
