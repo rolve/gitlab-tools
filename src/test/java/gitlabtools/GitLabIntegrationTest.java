071f9380084ef7c7dc50b71f2169b5c130e20d3b
Michael Faes <rolve@trick17.ch>
2022-05-12 15:17:52 +0200

    Other workaround...

--- src/test/java/gitlabtools/GitLabIntegrationTest.java
+++ src/test/java/gitlabtools/GitLabIntegrationTest.java
@@ -2,15 +2,29 @@ package gitlabtools;
 
 import gitlabtools.auth.TokenCreator;
 import org.apache.commons.lang3.ArrayUtils;
+import org.junit.jupiter.api.BeforeAll;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpTimeoutException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
 
 import static java.lang.String.join;
+import static java.lang.System.currentTimeMillis;
+import static java.net.http.HttpClient.Redirect.ALWAYS;
+import static java.net.http.HttpResponse.BodyHandlers.ofString;
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 public abstract class GitLabIntegrationTest {
+
+    private static final long MAX_WAIT = 300_000; // ms
+
     protected static String url = System.getenv("GITLAB_TOOLS_IT_URL");
     protected static String user = System.getenv("GITLAB_TOOLS_IT_USER");
     protected static String password = System.getenv("GITLAB_TOOLS_IT_PASSWORD");
@@ -52,6 +66,29 @@ public abstract class GitLabIntegrationTest {
             return file;
         } catch (IOException e) {
             throw new UncheckedIOException(e);
+        }
+    }
+
+    @BeforeAll
+    public static void waitForGitLabInstance() throws Exception {
+        var start = currentTimeMillis();
+        HttpResponse<String> response = null;
+        do {
+            var client = HttpClient.newBuilder()
+                    .followRedirects(ALWAYS)
+                    .build();
+            var request = HttpRequest.newBuilder(new URI(url))
+                    .timeout(Duration.of(5, SECONDS)).build();
+            try {
+                response = client.send(request, ofString());
+            } catch (HttpTimeoutException e) { /* ignore */ }
+        } while ((response == null || response.statusCode() != 200)
+                && currentTimeMillis() - start < MAX_WAIT);
+
+        if (response == null) {
+            throw new RuntimeException("could not connect to GitLab instance");
+        } else if (response.statusCode() != 200) {
+            throw new RuntimeException("could not connect to GitLab instance (status: " + response.statusCode() + ")");
         }
     }
 }
