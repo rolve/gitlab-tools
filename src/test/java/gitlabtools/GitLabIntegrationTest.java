8ba3d70c0a1db35263f3d2a872e45aafeefa05af
Michael Faes <rolve@trick17.ch>
2022-09-22 16:28:49 +0200

    Try to simplify integration tests using Testcontainers

--- src/test/java/gitlabtools/GitLabIntegrationTest.java
+++ src/test/java/gitlabtools/GitLabIntegrationTest.java
@@ -1,53 +1,30 @@
 package gitlabtools;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.time.Duration;
 
-import static java.lang.System.currentTimeMillis;
-import static java.net.http.HttpClient.Redirect.ALWAYS;
-import static java.net.http.HttpResponse.BodyHandlers.discarding;
-import static java.time.temporal.ChronoUnit.SECONDS;
-
+@Testcontainers
 public abstract class GitLabIntegrationTest {
 
-    private static final long MAX_WAIT = 20 * 60 * 1000; // 20 minutes!..
+    protected static String url;
+    protected static final String user = "root";
+    protected static final String password = "password";
 
-    protected static String url = System.getenv("GITLAB_TOOLS_IT_URL");
-    protected static String user = System.getenv("GITLAB_TOOLS_IT_USER");
-    protected static String password = System.getenv("GITLAB_TOOLS_IT_PASSWORD");
+    @Container
+    private static final GenericContainer<?> gitlab = new GenericContainer<>("gitlab/gitlab-ce:14.10.5-ce.0")
+            .withEnv("GITLAB_HTTPS", "false")
+            .withEnv("GITLAB_ROOT_PASSWORD", password)
+            .withExposedPorts(80)
+            .waitingFor(Wait.forHttp("/"))
+            .withStartupTimeout(Duration.ofMinutes(5));
 
     @BeforeAll
-    public static void waitForGitLabInstance() throws Exception {
-        var start = currentTimeMillis();
-        var client = HttpClient.newBuilder()
-                .followRedirects(ALWAYS)
-                .build();
-        var request = HttpRequest.newBuilder(new URI(url))
-                .timeout(Duration.of(5, SECONDS)).build();
-        HttpResponse<Void> response = null;
-        do {
-            try {
-                response = client.send(request, discarding());
-            } catch (IOException e) { /* ignore for now */ }
-        } while ((response == null || response.statusCode() != 200)
-                && currentTimeMillis() - start < MAX_WAIT);
-
-        if (response == null) {
-            // try one last time, without ignoring exceptions
-            try {
-                response = client.send(request, discarding());
-            } catch (IOException e) {
-                throw new RuntimeException("could not connect to GitLab instance", e);
-            }
-        }
-        if (response.statusCode() != 200) {
-            throw new RuntimeException("could not connect to GitLab instance (status: " + response.statusCode() + ")");
-        }
+    public static void setUrl() {
+        url = "http://" + gitlab.getHost() + ":" + gitlab.getMappedPort(80);
     }
 }
