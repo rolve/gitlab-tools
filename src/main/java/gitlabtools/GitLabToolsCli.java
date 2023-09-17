6d6e8a59273efe01fdd9e568e328ed3b4961249a
Michael Faes <rolve@trick17.ch>
2023-09-17 14:25:18 +0200

    Remove undocumented and not-used-in-years batch functionality

--- src/main/java/gitlabtools/GitLabToolsCli.java
+++ src/main/java/gitlabtools/GitLabToolsCli.java
@@ -4,12 +4,12 @@ import com.lexicalscope.jewel.cli.ArgumentValidationException;
 import gitlabtools.cmd.*;
 
 import java.lang.management.ManagementFactory;
-import java.util.*;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
-import static java.util.Arrays.stream;
-import static java.util.regex.Pattern.compile;
-import static java.util.stream.Collectors.toList;
-import static org.apache.commons.lang3.ArrayUtils.*;
+import static java.lang.String.join;
+import static org.apache.commons.lang3.ArrayUtils.subarray;
 
 public class GitLabToolsCli {
 
@@ -42,27 +42,9 @@ public class GitLabToolsCli {
             return;
         }
 
-        int batchIndex = indexOf(args, "--batch");
-        if (batchIndex == INDEX_NOT_FOUND) {
-            var cmdArgs = subarray(args, 1, args.length);
-            var cmd = constructCmd(cmdName, cmdArgs);
-            confirm(cmdName, List.<String[]>of(cmdArgs));
-            cmd.execute();
-        } else {
-            var constArgs = subarray(args, 1, batchIndex);
-            var varArgs = subarray(args, batchIndex + 1, args.length);
-            var completeArgs = stream(varArgs)
-                    .map(a -> addAll(constArgs, split(a)))
-                    .collect(toList());
-            var cmds = new ArrayList<Cmd<?>>();
-            for (var cmdArgs : completeArgs) {
-                cmds.add(constructCmd(cmdName, cmdArgs));
-            }
-            confirm(cmdName, completeArgs);
-            for (var cmd : cmds) {
-                cmd.execute();
-            }
-        }
+        var cmd = constructCmd(cmdName, subarray(args, 1, args.length));
+        confirm(args);
+        cmd.execute();
     }
 
     private static Cmd<?> constructCmd(String name, String[] args) throws Exception {
@@ -79,12 +61,9 @@ public class GitLabToolsCli {
      * To prevent accidental execution of a command within an IDE (and possibly
      * disastrous consequences), require confirmation.
      */
-    private static void confirm(String cmd, List<String[]> argsList) {
+    private static void confirm(String[] args) {
         if (runFromIde()) {
-            System.out.print("About to execute" + (argsList.size() == 1 ? " " : "\n"));
-            for (var args : argsList) {
-                System.out.println(cmd + " " + String.join(" ", args));
-            }
+            System.out.println("About to execute " + join(" ", args));
             System.out.print("Press Enter to continue.");
             try {
                 new Scanner(System.in).nextLine();
@@ -98,24 +77,5 @@ public class GitLabToolsCli {
         // assuming IDE attaches a Java agent (IntelliJ does...)
         var args = ManagementFactory.getRuntimeMXBean().getInputArguments();
         return args.stream().anyMatch(a -> a.startsWith("-javaagent:"));
-    }
-
-    private static String[] split(String args) {
-        var doubleQuot = "\"([^\"\\\\]*(?:\\\\.[^\"\\\\]*)*)\"";
-        var singleQuot = "'([^'\\\\]*(?:\\\\.[^'\\\\]*)*)'";
-        var spaceless = "[^\\s]+";
-        var regex = compile(doubleQuot + "|" + singleQuot + "|" + spaceless);
-        var matcher = regex.matcher(args);
-        var list = new ArrayList<String>();
-        while (matcher.find()) {
-            if (matcher.group(1) != null) {
-                list.add(matcher.group(1));
-            } else if (matcher.group(2) != null) {
-                list.add(matcher.group(2));
-            } else {
-                list.add(matcher.group());
-            }
-        }
-        return list.toArray(String[]::new);
     }
 }
